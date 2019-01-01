72c9c8af46f46bca37b00e5c306f44bd687997c2
charles ligony <c-ligony@efluid.fr>
2019-01-01 18:42:34 +0100

    [fix] bug

--- src/main/java/com/ircserv/contstante/Constante.java
+++ src/main/java/com/ircserv/contstante/Constante.java
@@ -3,5 +3,4 @@ package com.ircserv.contstante;
 public class Constante {
     public static int PORT = 186;
     public static String IP = "localhost";//"home.rscharff.fr";
-    public static String IP = "home.rscharff.fr";
 }
